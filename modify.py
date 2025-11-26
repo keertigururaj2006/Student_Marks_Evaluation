37d9c5ce8be9aed79eb04b87ee15b85a7a31567b
yashvanttalawar6-creator <yashvanttalawar6@gmail.com>
2025-11-26 19:25:57 +0530

    Saving Min_MAx for the code

---
+++ modify.py
@@ -0,0 +1,11 @@
+scores = input("Enter scores separated by space: ").split()
+scores = [float(x) for x in scores]
+
+total = sum(scores)
+avg = total / len(scores)
+
+print("Scores:", scores)
+print("Sum:", total)
+print("Average:", avg)
+print("Maximum:", max(scores))
+print("Minimum:", min(scores))
