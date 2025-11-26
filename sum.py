fdf5fc1e13a8de983ef4e7526277198c8a43e187
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-26 19:35:49 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,5 +1,12 @@
-scores = input("Enter scores separated by space: ").split()
-scores = [float(x) for x in scores]
+import sys
+
+# Check if arguments were passed
+if len(sys.argv) < 2:
+    print("Usage: python scores_main.py <s1> <s2> <s3> ...")
+    sys.exit(1)
+
+# Convert arguments to numbers
+scores = [float(x) for x in sys.argv[1:]]
 
 total = sum(scores)
 avg = total / len(scores)
